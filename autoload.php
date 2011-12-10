bf3b094e94035a061c3b6f4da5c45c6de90d4b40
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2011-12-10 16:37:24 -0600

    refactor autoloading

---
+++ autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespaces(array(
+		'Symfony'          => 'vendor/symfony/symfony/src',
+		'Doctrine\\Common' => 'vendor/doctrine/common/lib',
+		'Doctrine\\DBAL'   => 'vendor/doctrine/dbal/lib',
+		'Application'      => '.',
+		'Tests'            => '.',
+		'Modules'          => '.',
+));
+
+$loader->registerPrefixes(array(
+		'Twig_'  => 'vendor/twig/twig/lib',
+		'Zend_'  => 'vendor/',
+));
+
+$loader->register();
