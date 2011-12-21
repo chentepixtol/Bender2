c693305e5f19b1218e826383ff81e104ce815242
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2011-12-20 18:45:16 -0600

    update dependencies

--- autoload.php
+++ autoload.php
@@ -1,17 +1,23 @@
 <?php
 
-require_once 'vendor/symfony/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once 'vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-		'Symfony'          => 'vendor/symfony/symfony/src',
-		'Doctrine\\Common' => 'vendor/doctrine/common/lib',
-		'Doctrine\\DBAL'   => 'vendor/doctrine/dbal/lib',
-		'Application'      => '.',
-		'Test'            => '.',
-		'Modules'          => '.',
+		'Symfony\\Component\\Console'             => 'vendor/symfony/console',
+        'Symfony\\Component\\ClassLoader'         => 'vendor/symfony/class-loader',
+        'Symfony\\Component\\Config'              => 'vendor/symfony/config',
+        'Symfony\\Component\\DependencyInjection' => 'vendor/symfony/dependency-injection',
+        'Symfony\\Component\\EventDispatcher'     => 'vendor/symfony/event-dispatcher',
+        'Symfony\\Component\\Finder'              => 'vendor/symfony/finder',
+        'Symfony\\Component\\Yaml'                => 'vendor/symfony/yaml',
+		'Doctrine\\Common'             => 'vendor/doctrine/common/lib',
+		'Doctrine\\DBAL'               => 'vendor/doctrine/dbal/lib',
+		'Application' => '.',
+		'Test'        => '.',
+		'Modules'     => '.',
 ));
 
 $loader->registerPrefixes(array(
