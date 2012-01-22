64571ab7f9999104b40f1e94188df6a6784803fa
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2012-01-21 18:11:19 -0600

    include_path fix

--- autoload.php
+++ autoload.php
@@ -4,6 +4,8 @@ require_once 'vendor/.composer/autoload.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
+set_include_path(get_include_path() . PATH_SEPARATOR . 'vendor');
+
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Application' => '.',
