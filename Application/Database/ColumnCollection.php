bf202ffe8185490bf3cbf65a9e1cf0650be62b8f
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2012-03-03 20:16:14 -0600

    add select options in the forms

--- Application/Database/ColumnCollection.php
+++ Application/Database/ColumnCollection.php
@@ -20,6 +20,16 @@ class ColumnCollection extends BaseCollection
      *
      * @return \Application\Database\ColumnCollection
      */
+    public function inForeignKeys(){
+        return $this->filter(function(Column $column){
+            return $column->getTable()->getForeignKeys()->containsColumnName($column->getIndex());
+        });
+    }
+
+    /**
+     *
+     * @return \Application\Database\ColumnCollection
+     */
     public function nonPrimaryKeys()
     {
         return $this->filter(function(Column $column){
@@ -36,6 +46,32 @@ class ColumnCollection extends BaseCollection
         return $this->filter(function(Column $column){
             return !$column->getTable()->getForeignKeys()->containsColumnName($column->getIndex());
         });
+    }
+
+    /**
+     *
+     * @param string $regex
+     * @return boolean
+     */
+    public function hasColumnName($regex){
+        return $this->getByColumnName($regex) !== false;
+    }
+
+    /**
+     *
+     * @param string $regex
+     * @return \Application\Database\Column
+     */
+    public function getByColumnName($regex){
+        $this->rewind();
+        while( $this->valid() ) {
+            $column = $this->read();
+            if( preg_match($regex, $column->getName()->toString()) ){
+                return $column;
+            }
+        }
+        $this->rewind();
+        return false;
     }
 
 }
